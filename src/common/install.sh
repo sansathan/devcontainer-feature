d0ba17d24dc29a96ad5b58591364fc023a7232c1
Ade M Ramdani <qcynaut@gmail.com>
2025-09-27 06:47:08 +0000

    fix: update version to 1.0.1 and add 'sag' to possible users in install script

--- src/common/install.sh
+++ src/common/install.sh
@@ -7,7 +7,7 @@ bash ./install_fish.sh
 bash ./install_starship.sh
 
 # Get all possible non-root users
-POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+POSSIBLE_USERS=("vscode" "node" "codespace" "sag" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
 # create .config/fish if it doesn't exist
 for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
   if id -u ${CURRENT_USER} > /dev/null 2>&1; then
