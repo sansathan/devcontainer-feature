68b29f85b7664df29e336b78c302e51117a2eb37
Ade M Ramdani <qcynaut@gmail.com>
2025-09-27 05:27:27 +0000

    refactor: remove checks for fish config file and starship initialization

--- test/common/test.sh
+++ test/common/test.sh
@@ -7,7 +7,5 @@ source dev-container-features-test-lib
 check "fish should be installed" bash -c "command -v fish"
 check "default shell should be fish" bash -c "echo \$SHELL | grep 'fish'"
 check "fish config directory exists" bash -c "[ -d ~/.config/fish ]"
-check "fish config file exists" bash -c "[ -f ~/.config/fish/config.fish ]"
-check "fish config file contains starship" bash -c "grep -q 'starship init fish' ~/.config/fish/config.fish"
 
 reportResults
